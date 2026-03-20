7266250a28643bb175d337513a4aa639b4f49e9a
Virat Kohli <ayushdiazoniumsalt@gmail.com>
2026-03-21 00:29:55 +0530

    change4

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ const __dirname=path.resolve();
 const PORT =  ENV.PORT || 3000;
 
 app.use(express.json({ limit : "5mb"})) ;// req.body
-app.use(cors({origin:process.ENV.CLIENT_URL, credentials:true}));
+app.use(cors({origin:process.env.CLIENT_URL, credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes); 
